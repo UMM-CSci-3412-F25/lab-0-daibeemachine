d5413712426f2b125cf6289df72168c5def2c0f9
daibeemachine <ols00559@morris.umn.edu>
2025-08-27 02:44:01 -0500

    I got the extract_and_compiile shellscript file to work! I added a few commands to compile a executable file from the NthPrime folder and run it with the given parameter.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -6,11 +6,12 @@ FOLDER="$COMPRESSED_FILE"
 FOLDER+="/NthPrime"
 COMPRESSED_FILE+=/NthPrime.tgz
 
-echo $(dirname "$(realpath "$0")")
-
 cd $(dirname "$(realpath "$0")")
 tar zxf "$COMPRESSED_FILE"
-ls
+cd "$FOLDER"
+gcc main.c nth_prime.c -o program.exe
+./program.exe "$1"
+cd ..
 rm -rf $FOLDER
 cd "$WORKING_DIRECTORY"
 
