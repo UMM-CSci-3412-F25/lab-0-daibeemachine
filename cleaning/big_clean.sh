a62330d1ee014e47eac4ac33728f3458531dd512
daibeemachine <ols00559@morris.umn.edu>
2025-08-27 04:46:52 -0500

    Got big_clean.sh functional
    Within this commit, I created a functional version of big_clean.sh. Unfortunately, it looks like some of the bats tests aren't passing in both big_clean.sh and extract_and_compile.sh, mainly due to some directory issues. So, I'm going to fix that next.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
 
+WORKING_DIRECTORY=$(pwd)
+FILE_NAME="cleaned_"
+FILE_NAME+="$(basename $(realpath "$1"))"
 SCRATCH=$(mktemp --directory)
 
-tar zxf "$1" --directory "$SCRATCH"
-ls $SCRATCH
 
-rm -rf "$SCRATCH"
+echo $SCRATCH
+echo $FILE_NAME
+tar zxf "$1" --directory "$SCRATCH"
+grep -lr "DELETE ME!" "$SCRATCH" | xargs rm
+cd $SCRATCH
+tar -czf $FILE_NAME .
+ls
+mv "$FILE_NAME" "$WORKING_DIRECTORY"
+cd $WORKING_DIRECTORY
+
+rm -rf $SCRATCH
