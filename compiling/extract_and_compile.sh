bef877a58ad008cae2a43f594b1c3958dddfd0cc
daibeemachine <ols00559@morris.umn.edu>
2025-08-27 02:33:06 -0500

    Within this commit, I used the NthPrime folder I (accidentally) made outside of the compiling folder and compiled a "test" executable. I tested it with ./test 17, and it seems to work! (i.e., output is "Prime 17 = 59")

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+WORKING_DIRECTORY=$(pwd)
 COMPRESSED_FILE=$(dirname "$(realpath "$0")")
 FOLDER="$COMPRESSED_FILE"
 FOLDER+="/NthPrime"
 COMPRESSED_FILE+=/NthPrime.tgz
+
 echo $(dirname "$(realpath "$0")")
+
+cd $(dirname "$(realpath "$0")")
 tar zxf "$COMPRESSED_FILE"
 ls
 rm -rf $FOLDER
+cd "$WORKING_DIRECTORY"
+
