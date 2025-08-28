db12e70dc60535fc28c7b5ac5b0489cdc7d2648c
daibeemachine <ols00559@morris.umn.edu>
2025-08-28 02:07:15 -0500

    Fixed big_clean.sh
    Within this commit, I fixed big_clean.sh by having it compress a specific folder (the argument passed in) as opposed to simply compressing the entire directory with "."

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -3,17 +3,13 @@
 WORKING_DIRECTORY=$(pwd)
 FILE_NAME="cleaned_"
 FILE_NAME+="$(basename $(realpath "$1"))"
+FILE_NAME_ONLY="$(basename $(realpath "$1") .tgz)"
 SCRATCH=$(mktemp --directory)
-
-
 echo $SCRATCH
 echo $FILE_NAME
 tar zxf "$1" --directory "$SCRATCH"
 grep -lr "DELETE ME!" "$SCRATCH" | xargs rm
 cd $SCRATCH
-tar -czf $FILE_NAME .
-ls
+tar -czf $FILE_NAME "$FILE_NAME_ONLY"
 mv "$FILE_NAME" "$WORKING_DIRECTORY"
 cd $WORKING_DIRECTORY
-
-rm -rf $SCRATCH
